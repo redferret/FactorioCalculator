8919cff472264621b7c906ede2a7545d841b0454
Richard DeSilvey <rdesilvey@gmail.com>
2018-08-19 19:09:19 -0600

    Added Constants
    Actions and Routes

---
+++ resources/assets/js/routes.js
@@ -0,0 +1,6 @@
+
+import { RootElement } from './bootstrap.js';
+
+export const ROOT                   = RootElement.getAttribute('url');
+export const BALANCE_PRODUCTION     = ROOT + '/balance';
+export const GET_FACTORIES          = ROOT + '/factories';
