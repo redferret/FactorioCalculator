68a2f8eec617e37dd91a63648a169f6383d744d0
Richard DeSilvey <rdesilvey@gmail.com>
2018-08-29 13:57:27 -0600

    UI/UX

--- resources/assets/js/components/game-items.js
+++ resources/assets/js/components/game-items.js
@@ -76,11 +76,19 @@ export default class GameItems extends React.Component {
               </ButtonToolbar>
             </Well>
             <Well>
-              <h3><Label bsStyle='success'>Game Producers</Label></h3>
-              <ListGroup>
-                <ListGroupItem>Item 1</ListGroupItem>
-                <ListGroupItem>Item 2</ListGroupItem>
-              </ListGroup>
+              <PanelGroup>
+                <Panel bsStyle='success'>
+                  <Panel.Heading>
+                    Game Producers
+                  </Panel.Heading>
+                  <Panel.Body>
+                    <ListGroup>
+                      <ListGroupItem>Item 1</ListGroupItem>
+                      <ListGroupItem>Item 2</ListGroupItem>
+                    </ListGroup>
+                  </Panel.Body>
+                </Panel>
+              </PanelGroup>
               <ButtonToolbar>
                 <Button>Add Producer</Button>
               </ButtonToolbar>
