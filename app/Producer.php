01f9821752e9d5afb0e3c2596947c99b3b0c763e
Richard DeSilvey <rdesilvey@gmail.com>
2018-09-01 10:54:01 -0600

    Name Fillable

--- app/Producer.php
+++ app/Producer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Producer extends Model {
 
   protected $fillable = ['producer_type', 'image_file', 'speed',
-    'power', 'production_line_id', 'process'];
+    'power', 'production_line_id', 'process', 'name'];
 
   public function productionLine() {
     return $this->belongsTo(ProductionLine::class);
