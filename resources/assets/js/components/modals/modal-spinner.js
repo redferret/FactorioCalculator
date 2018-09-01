9538e17e181bf26f7cb31df4adfb2edef5811963
Richard DeSilvey <rdesilvey@gmail.com>
2018-09-01 00:45:16 -0600

    UI for new Spinner in modal

--- resources/assets/js/components/modals/modal-spinner.js
+++ resources/assets/js/components/modals/modal-spinner.js
@@ -3,11 +3,12 @@ import Input from '../input.js';
 import ModalsStore from '../../stores/modals-store.js';
 import NewFactoryModalStore from '../../stores/new-factory-modal-store.js';
 import React from 'react';
-// import ReactSpinner from 'reactjs-spinner';
+import { css } from 'react-emotion';
+// First way to import
+import { BeatLoader } from 'react-spinners';
 
 import { Modal } from 'react-bootstrap';
 import { SPINNER_MODAL_ID } from '../../constants.js';
-
 
 export default class ModalSpinner extends React.Component {
   constructor(props, context) {
@@ -45,6 +46,13 @@ export default class ModalSpinner extends React.Component {
         >
         <Modal.Body>
           Sending Request... Please wait
+          <br/>
+          <BeatLoader
+            sizeUnit={"px"}
+            size={20}
+            color={'#80aef7'}
+            loading={true}
+          />
         </Modal.Body>
       </Modal>
     );
