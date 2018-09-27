b3daf80d7e3546c0e3b4386d3d253bf8b5df38cd
Richard DeSilvey <rdesilvey@gmail.com>
2018-09-27 15:46:17 -0600

    Notification updates

--- app/Notifications/MailResetPasswordNotification.php
+++ app/Notifications/MailResetPasswordNotification.php
@@ -42,12 +42,11 @@ class MailResetPasswordNotification extends Notification
         $link = url( "/password/reset/?token=" . $this->token );
 
         return ( new MailMessage )
-           ->view('reset.emailer')
            ->from(env('MAIL_USERNAME', 'support@richard-desilvey.info'))
-           ->subject( 'Reset your password' )
-           ->action( 'Reset Password', $link )
-           ->attach('reset.attachment')
-           ->line( 'Thank you!' );
+           ->subject('Reset your password')
+           ->line('You are receiving this email because we received a password reset request for your account.')
+           ->action('Reset Password', url(config('app.url').route('password.reset', $this->token, false)))
+           ->line('If you did not request a password reset, no further action is required.');
      }
 
     /**
