ba64b1d4d9fafe530d0d867e476a262e2ea310a0
Richard DeSilvey <rdesilvey@gmail.com>
2018-09-27 15:27:57 -0600

    Password Resets

--- app/User.php
+++ app/User.php
@@ -9,6 +9,16 @@ class User extends Authenticatable {
   use Notifiable;
 
   /**
+   * Send the password reset notification.
+   *
+   * @param  string  $token
+   * @return void
+   */
+  public function sendPasswordResetNotification($token) {
+      $this->notify(new App\Notifications\MailResetPasswordNotification($token));
+  }
+
+  /**
    * The attributes that are mass assignable.
    *
    * @var array
